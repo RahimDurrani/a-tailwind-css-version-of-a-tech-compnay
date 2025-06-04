e50575ab5b6100d834240883813752dc739a895c
RahimDurrani <101318092+RahimDurrani@users.noreply.github.com>
2025-06-04 12:13:22 +0500

    navigation click removal added

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
-    // Toggle mobile menu visibility
-       const toggle = document.getElementById('menu-toggle');
-    const menu = document.getElementById('menu');
+const toggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+const menuItems = menu.querySelectorAll('a'); 
 
-    toggle.addEventListener('click', () => {
-      const isOpen = toggle.classList.contains('open');
+toggle.addEventListener('click', () => {
+  const isOpen = toggle.classList.contains('open');
 
-      toggle.classList.toggle('open');
-      toggle.classList.toggle('group-[.open]');
+  toggle.classList.toggle('open');
+  toggle.classList.toggle('group-[.open]');
 
-      if (!isOpen) {
-        menu.classList.remove('max-h-0', 'opacity-0');
-        menu.classList.add('max-h-96', 'opacity-100');
-      } else {
-        menu.classList.add('max-h-0', 'opacity-0');
-        menu.classList.remove('max-h-96', 'opacity-100');
-      }
-    });
+  if (!isOpen) {
+    menu.classList.remove('max-h-0', 'opacity-0');
+    menu.classList.add('max-h-96', 'opacity-100');
+  } else {
+    menu.classList.add('max-h-0', 'opacity-0');
+    menu.classList.remove('max-h-96', 'opacity-100');
+  }
+});
+
+
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    toggle.classList.remove('open');
+    toggle.classList.remove('group-[.open]');
+    menu.classList.add('max-h-0', 'opacity-0');
+    menu.classList.remove('max-h-96', 'opacity-100');
+  });
+});
+
 
   const swiper = new Swiper('.mySwiper', {
     loop: true,
